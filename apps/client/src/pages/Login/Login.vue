1b986bac9a3cacfa5c1f0d4361a17d7c17af2a69
Kacper Kukuła <kacper.kukula96@gmail.com>
2025-01-04 22:29:13 +0000

    firebase init, login in

--- apps/client/src/pages/Login/Login.vue
+++ apps/client/src/pages/Login/Login.vue
@@ -2,20 +2,48 @@
     <section id="login">
         <SimpleTile>
             <h1>Login</h1>
-            <input type="text" placeholder="Username">
-            <input type="password" placeholder="Password">
+            <input type="text" v-model="email" placeholder="Email">
+            <input type="password" v-model="password" placeholder="Password">
+
+            <button @click="loginUser">Login</button>
         </SimpleTile>
     </section>
 </template>
 
 <script lang="ts">
+import { defineComponent, ref } from 'vue';
 import SimpleTile from '@/components/SimpleTile/SimpleTile.vue';
-import { defineComponent } from 'vue';
+import { service as firebaseService } from '@/modules/firebase/firebase';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 export default defineComponent({
     name: 'Login',
     components: {
         SimpleTile
+    },
+    setup() {
+        const auth = getAuth(firebaseService);
+
+        const email = ref<string>('');
+        const password = ref<string>('');
+        const errorMessage = ref<string | null>(null);
+
+        const loginUser = async () => {
+
+            try {
+                const userCredential = await signInWithEmailAndPassword(auth, email.value, password.value);
+                const user = userCredential.user;
+                console.log('User logged in:', user);
+            } catch (error) {
+                console.error('Error logging in:', error);
+            }
+        };
+
+        return {
+            email,
+            password,
+            loginUser,
+        }
     }
 })
 </script>
