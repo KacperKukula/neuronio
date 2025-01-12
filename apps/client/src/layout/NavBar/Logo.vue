852a787dafd343ae7480ac7204300733d6f30f9c
Kacper Kukuła <kacper.kukula96@gmail.com>
2025-01-12 11:09:08 +0000

    added logo component

---
+++ apps/client/src/layout/NavBar/Logo.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="logo">
+        <BeakerIcon />
+
+        BrainLab
+    </div>
+</template>
+
+<script setup lang="ts">
+import { BeakerIcon } from '@heroicons/vue/24/outline';
+
+</script>
+
+<style lang="scss" scoped>
+.logo {
+    box-sizing: border-box;
+    padding: 6px;
+    color: white;
+    display: flex;
+    gap: 4px;
+
+    > svg {
+        height: 1.6rem;
+    }
+}
+</style>
