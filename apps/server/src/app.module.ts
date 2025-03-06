eb1c418cc3a100632917cc41fe87dcff8c985dda
Kacper Kukuła <kacper.kukula96@gmail.com>
2025-03-06 23:05:05 +0000

    connect to database

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -2,9 +2,26 @@ import { MiddlewareConsumer, Module, NestModule, Req, RequestMethod } from '@nes
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './conf/typeorm.config';
+import { UserModule } from './mods/user/user.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: typeOrmConfig,
+    }),
+
+    /* Functional modules */
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
