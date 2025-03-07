fdcd5d210b06eb6bbc66b7564d5eb4e5d1af954d
Kacper Kukuła <kacper.kukula96@gmail.com>
2025-03-07 16:56:24 +0000

    first login functionality

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,11 +1,11 @@
 import { MiddlewareConsumer, Module, NestModule, Req, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './modules/auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './conf/typeorm.config';
-import { UserModule } from './mods/user/user.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
---
+++ apps/server/src/modules/auth/auth.service.ts
@@ -0,0 +1,23 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '@/entities/user.entity';
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+
+import { LoginDto } from './dtos/LoginDto';
+
+@Injectable()
+export class AuthService {
+    constructor(
+    @InjectRepository(User)
+        private userRepository: Repository<User>,
+    ) {}
+
+    async login(loginDto: LoginDto): Promise<User> {
+        
+        const userPromise = await this.userRepository.findOne({
+            where: { email: loginDto.email }
+        });
+
+        return userPromise;
+    }
+}
